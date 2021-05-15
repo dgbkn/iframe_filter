09fd25bdaf0910dae7c9b58f5e28c9435ddc821a
dgbkn <65810521+dgbkn@users.noreply.github.com>
2021-05-15 10:22:12 +0530

    Update sandbox.php

--- sandbox.php
+++ sandbox.php
@@ -1,6 +1,6 @@
 <?php
 $furl = $_GET['furl'];
 
-echo '<iframe sandbox="allow-scripts" src="'.$furl.'" style="width:100%;height:100%" frameborder="none"></iframe>';
+echo '<iframe sandbox="allow-forms allow-scripts allow-same-origin allow-top-navigation" src="'.$furl.'" style="width:100%;height:100%" frameborder="none" allowfullscreen></iframe>';
 
 ?>
