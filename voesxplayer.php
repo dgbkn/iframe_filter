dba36267532ab35537603004e2d7b6cea86c1074
dgbkn <65810521+dgbkn@users.noreply.github.com>
2021-05-17 08:02:23 +0530

    Update voesxplayer.php

--- voesxplayer.php
+++ voesxplayer.php
@@ -2,7 +2,7 @@
 //Get the raw html.
 $furl="https://voe.sx/e/". $_GET["url"];
 $raw = file_get_contents($furl);
-$m3u  =  preg_match("(http.*?\.m3u8[^&">]+)",$raw);
+$m3u  =  preg_match('(http.*?\.m3u8[^&">]+)',$raw);
 
 
 echo $m3u;
