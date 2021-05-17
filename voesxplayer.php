d2b4aed6f12999622752518e832602482f48c361
dgbkn <65810521+dgbkn@users.noreply.github.com>
2021-05-17 09:43:34 +0530

    Update voesxplayer.php

--- voesxplayer.php
+++ voesxplayer.php
@@ -1,8 +1,11 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 //Get the raw html.
-$furl="https://voe.sx/e/". $_GET["url"];
+$furl= $_GET["url"];
 $raw = file_get_contents($furl);
-$m3u  =  preg_match('(http.*?\.m3u8[^&">]+)',$raw);
+$m3u  =  preg_match('(https.*?\.m3u8[^&">]+)',$raw);
 
 
 echo $m3u;
