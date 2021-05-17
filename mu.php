869dc5fd9be72c6ab9e9a774744487fb451b07e4
dgbkn <65810521+dgbkn@users.noreply.github.com>
2021-05-17 09:49:15 +0530

    Update mu.php

--- mu.php
+++ mu.php
@@ -6,7 +6,11 @@ error_reporting(E_ALL);
 $furl= $_GET["url"];
 $raw = file_get_contents($furl);
 $m3u  =  preg_match('(https.*?\.m3u8[^&">]+)',$raw);
+preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $raw, $match);
 
+echo "<pre>";
+print_r($match[0]); 
+echo "</pre>";
 
 echo $m3u;
 ?>
