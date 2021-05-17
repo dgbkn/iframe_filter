e421d95bf4e002528cce244043fac3e4879ceb4b
dgbkn <65810521+dgbkn@users.noreply.github.com>
2021-05-17 09:57:53 +0530

    Update mu.php

--- mu.php
+++ mu.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 //Get the raw html.
 $furl= $_GET["url"];
 $raw = file_get_contents($furl);
-preg_match_all('/"([^"]+)"/', $raw, $m)
+preg_match_all('/"([^"]+)"/', $raw, $m);
 $m3u  =  preg_match('(https.*?\.m3u8[^&">]+)',$raw);
 preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $raw, $match);
 
